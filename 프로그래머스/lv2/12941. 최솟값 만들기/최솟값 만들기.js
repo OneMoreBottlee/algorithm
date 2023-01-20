96d18fab8530a5e2907965250c0c612d872f7a25
onemorebottlee <hanbj0301@gmail.com>
2023-01-21 00:25:00 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.90 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,5 +1,5 @@
 function solution(A,B){
   A.sort((a,b)=>a-b)
-  B.sort((a,b)=>a-b)
-  return A.map((el,index)=> el*B[B.length-1-index]).reduce((acc,cur)=>acc+cur)
+  B.sort((a,b)=>b-a)
+  return A.map((el,index)=> el*B[index]).reduce((acc,cur)=>acc+cur)
 }
