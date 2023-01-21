435ea6fe113b81818b4d33f5794878fbc084cdfb
onemorebottlee <hanbj0301@gmail.com>
2023-01-21 21:22:26 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 2.45 ms, Memory: 34.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -0,0 +1,14 @@
+function solution(s){
+	let count = 0
+  let countZero = 0
+  
+  while(s !== "1"){
+    let length = s.length
+    s = s.replaceAll("0", "")
+  	count++
+    countZero += length - s.length
+    s = s.length.toString(2)
+  }
+    
+  return [count, countZero]
+}
