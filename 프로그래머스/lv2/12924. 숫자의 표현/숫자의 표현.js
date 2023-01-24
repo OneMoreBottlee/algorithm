65d8e5300a5d44b9e4c24aa4f03a9767dcd92827
onemorebottlee <hanbj0301@gmail.com>
2023-01-24 19:06:21 +0900

    Update 숫자의 표현.js
    🛠 수도 코드 추가 & 코드명 수정

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,19 +1,27 @@
 function solution(n) {
   // n만 있을때 조건 성립
   let result = 1
-	let count = 1
+  let startValue = 1
   
-  while(count <= Math.floor(n/2)){
+  // 중앙값보다 큰 값은 확인할 필요 X
+  while(startValue <= Math.floor(n/2)){
     let sum = 0
-    for(let i = count; i <= n; i++){
+    
+    // 시작값부터 n의 중앙값까지 조건을 만족하는 값 찾기
+    for(let i = startValue; i <= n; i++){
       sum += i
+      // 더했을때 n보다 큰 값은 필요없음, 반복문 종료
       if(sum > n)break
+      
+      // 조건을 만족하는 값. result 1 추가 & 반복문 종료
       if(sum === n){
         result++
         break
       }
     }
-    count++
+    
+    // 시작값 1추가
+    startValue++
   }
   return result
 }
