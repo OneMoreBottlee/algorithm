1ca72f0c1077490c755e26f2de6a21a271f35b75
Han Byoung Ju <hanbj0301@gmail.com>
2023-01-27 16:10:46 +0900

    [Bronze V] Title: 학점계산, Time: 144 ms, Memory: 9580 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.js
@@ -0,0 +1,13 @@
+const input = require('fs').readFileSync("/dev/stdin").toString()
+
+let result = "0.0"
+
+if(input[0] === "A")result = "4.0"
+else if(input[0] === "B")result = "3.0"
+else if(input[0] === "C")result = "2.0"
+else if(input[0] === "D")result = "1.0"
+
+if(input[1] === "+")result = result*1 + 0.3
+else if(input[1] === "-")result = result*1 - 0.3
+
+console.log(result.toString())
