1183ff7c2e5a61c862869457fd6bd15eaf90870f
Han Byoung Ju <hanbj0301@gmail.com>
2023-02-13 00:13:28 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,6 +1,20 @@
 function solution(strings, n) {
+    let result = [];
   
-	const str = strings.sort((a,b) => a[n] === b[n] ? a.localeCompare(b) : a[n].localeCompare(b[n]))
+  	// 해당 요소 앞에 n 번째 문자 추가
+    for (let i = 0; i < strings.length; i++) {
+        let chu = strings[i][n];
+        strings[i] = chu + strings[i];
+    }
   
-  return str
+  	// 정렬
+    strings.sort();
+  
+  	// 지우기 & result 에 추가
+    for (var j = 0; j < strings.length; j++) {
+        strings[j] = strings[j].replace(strings[j][0],"");
+        result.push(strings[j])
+    }
+
+    return result;
 }
