3f6987441f25c6d318f3cd6766e41bab77c4dd7f
Han Byoung Ju <hanbj0301@gmail.com>
2023-02-16 11:24:30 +0900

    [Silver II] Title: A → B, Time: 128 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.js
@@ -0,0 +1,25 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ');
+
+const A = input[0]*1
+const B = input[1]*1
+
+function AB (a,b){
+    let count = 1 
+    
+    while(a < b){
+      // 짝수
+      if(b % 2 === 0){
+        b = b/2
+        count++
+        
+      }else{ //홀수
+        b = b.toString()
+        b[b.length-1] === "1" ? (b = b.slice(0,b.length-1)*1, count++) : (a=b, count=-1)
+      }      
+    }
+  
+	if(a > b){a=b, count=-1}
+    return count
+}
+
+console.log(AB(A,B))
