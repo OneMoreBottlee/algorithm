6836125a7de50a6550acf86ccaa0f6895569840a
Han Byoung Ju <hanbj0301@gmail.com>
2023-02-19 23:26:44 +0900

    [unrated] Title: 카드 뭉치, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/README.md
+++ 프로그래머스/unrated/159994. 카드 뭉치/README.md
@@ -1,10 +1,10 @@
 # [unrated] 카드 뭉치 - 159994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.04 ms
+메모리: 33.4 MB, 시간: 0.08 ms
 
 ### 구분
 
--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -1,19 +1,14 @@
 function solution(cards1, cards2, goal) {
-  let count = 0
-  const n = goal.length
   
-  while(count < n){
-    if(goal[0] === cards1[0]){
-      goal.shift()
+  for(const card of goal){
+    if(card === cards1[0]){
       cards1.shift()
-    }
-    if(goal[0] === cards2[0]){
-      goal.shift()
+    }else if(card === cards2[0]){
       cards2.shift()
+    }else{
+      return "No"
     }
-    
-    count++
   }
   
-  return goal.length===0 ? "Yes" : "No"
+  return "Yes"
 }
