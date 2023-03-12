ef27819df14d47cd4bd0f22dbfdd82373afae436
Han Byoung Ju <hanbj0301@gmail.com>
2023-03-12 18:34:23 +0900

    [Gold V] Title: 전깃줄, Time: 120 ms, Memory: 9368 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.js
+++ 백준/Gold/2565. 전깃줄/전깃줄.js
@@ -5,7 +5,6 @@ const dp = Array(N).fill(1)
 
 // 교차하지 않는 전깃줄 확인
 for(let i = 0; i < N; i++){
-    count = 0
     for(let j = 0; j < i; j++){
         if(line[i][1] > line[j][1]){
             dp[i] = Math.max(dp[i], dp[j]+1)
