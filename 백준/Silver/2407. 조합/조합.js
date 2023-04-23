fb8db5cdced992e087aaa5b9b8ccf4e69ad9611e
Han ByoungJu <hanbj0301@gmail.com>
2023-04-23 15:14:39 +0900

    [Silver III] Title: 조합, Time: 120 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Silver/2407. 조합/조합.js
+++ 백준/Silver/2407. 조합/조합.js
@@ -2,16 +2,14 @@ const [n, m] = require("fs").readFileSync("/dev/stdin").toString().trim().split(
 
 // nCr = n! / (r! * (n-r)!)
 function combination(n, r){
-    const result = factorial(n) / (factorial(r) * factorial(n-r))
-    return result
-}
-
-function factorial(n) {
     const dp = [1n]
+    
     for(let i = 1n; i <= BigInt(n); i++){
         dp[i] = dp[i-1n] * i
     }
-    return dp[n]
+
+    const result = dp[n] / (dp[r] * dp[n-r])
+    return result
 }
 
 console.log(combination(n, m).toString())
