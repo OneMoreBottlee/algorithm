93031285793dee931a45b00ba9ed68332217b2bc
Han ByoungJu <hanbj0301@gmail.com>
2023-04-28 09:30:32 +0900

    [Silver III] Title: N과 M (3), Time: 908 ms, Memory: 154240 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.js
@@ -0,0 +1,29 @@
+const [N, M] = require("fs").readFileSync("/dev/stdin").toString().trim().split(" ").map(Number)
+
+function solution(N,M) {
+    const seq = new Array(N).fill(0)
+	const visited = new Array(N).fill(false)
+    const result = []
+
+    function dfs(cnt){
+        if(cnt === M){
+            const arr = []
+            for(let i = 0; i < M; i++){
+                arr.push(seq[i])
+            }
+            return result.push(arr.join(" "))
+        }
+        
+        for(let i = 1; i <= N; i++){
+                visited[i] = true
+                seq[cnt] = i
+                dfs(cnt+1)
+                visited[i] = false
+        }
+    }
+    
+    dfs(0)
+    return result.join("\n")
+}
+
+console.log(solution(N,M))
