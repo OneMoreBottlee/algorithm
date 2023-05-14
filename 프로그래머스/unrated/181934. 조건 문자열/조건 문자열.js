46b0ac18e185ce3d5e1337eca0a478d5710d26c9
Han ByoungJu <onemorebottlee@gmail.com>
2023-05-14 21:51:49 +0900

    [unrated] Title: 조건 문자열, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.js
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.js
@@ -3,13 +3,13 @@ function solution(ineq, eq, n, m) {
     if(eq === "="){
       return n <= m ? 1 : 0
     }else{
-      return !(n < m) ? 0 : 1
+      return n < m ? 1 : 0
     }
   }else{
     if(eq === "="){
-      return n >= m ? 0 : 0
+      return n >= m ? 1 : 0
     }else{
-      return !(n > m) ? 0 : 1
+      return n > m ? 1 : 0
     }
   }
 }
