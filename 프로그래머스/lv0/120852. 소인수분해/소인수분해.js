dc7450b0559d7c6dc248561e17ab2b328b2027a4
Han ByoungJu <onemorebottlee@gmail.com>
2023-05-17 16:15:27 +0900

    [level 0] Title: 소인수분해, Time: 0.09 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120852. 소인수분해/소인수분해.js
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.js
@@ -1,7 +1,6 @@
 function solution(n) {
     const result = []
-    if(n%2 === 0)result.push(2)
-    for(let i = 3; i <= n; i += 2){
+    for(let i = 2; i <= n; i++){
         if(n%i === 0 && checkPrime(i)){
             n = n / i
             result.push(i)
