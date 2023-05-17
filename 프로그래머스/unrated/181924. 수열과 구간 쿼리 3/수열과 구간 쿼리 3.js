e343e1e29b2b3f79e4e2d810c1db72e020440d86
Han ByoungJu <onemorebottlee@gmail.com>
2023-05-17 22:17:50 +0900

    [unrated] Title: 수열과 구간 쿼리 3, Time: 0.38 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -1,8 +1,6 @@
 function solution(arr, queries) {
   queries.forEach(([i, j]) => {
-    const aa = arr[i]
-    arr[i] = arr[j]
-    arr[j] = aa
+    [arr[i], arr[j]] = [arr[j], arr[i]]
   })
                   
   return arr
