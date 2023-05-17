762d1ec527fde823cd75ee2e68c9f1068a28d8e9
Han ByoungJu <onemorebottlee@gmail.com>
2023-05-17 21:49:39 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 3.53 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -1,12 +1,9 @@
 function solution(arr, queries) {
-  	    return queries.reduce((acc, cur) => {
-          const range = arr.slice(cur[0], cur[1]+1)
-          const bigger = range.filter(el => el > cur[2])
-          if(!bigger.length)return [...acc, -1]
-          return [...acc, Math.min(...bigger)]
-        }, [])
-  
-    // return queries.map(el => arr.slice(el[0], el[1]+1)
-    //                    .filter(ele => ele > el[2])
-    //                    .sort()[0] ?? -1)  
+    return queries.map(([s, e, k]) => {
+      const range = arr.slice(s, e+1)
+      const bigger = range.filter(ele => ele > k)
+      
+      if(!bigger.length)return -1
+      return Math.min(...bigger)
+      })  
 }
