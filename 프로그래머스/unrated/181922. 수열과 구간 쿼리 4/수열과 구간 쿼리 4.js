b2a36c9b750d3a47f2462c30776cb9daee35eb2e
Han ByoungJu <onemorebottlee@gmail.com>
2023-05-18 14:51:43 +0900

    [unrated] Title: 수열과 구간 쿼리 4, Time: 2.60 ms, Memory: 35.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -0,0 +1,8 @@
+function solution(arr, queries) {
+    queries.forEach(([s,e,k]) => {
+        for(let i = s; i <= e; i++){
+            if(i % k === 0) arr[i] += 1
+        }
+    })
+    return arr
+}
