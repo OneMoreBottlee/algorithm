305c61ba0bf895b7be08895c6afd7020c0b2e0bc
Han ByoungJu <onemorebottlee@gmail.com>
2023-05-20 17:37:47 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,7 @@
+function solution(my_string, num1, num2) {
+    const arr = my_string.split("")
+    
+    const change = [arr[num1], arr[num2]] = [arr[num2], arr[num1]]
+    
+  	return arr.join("")
+}
