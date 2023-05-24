38a07499f7e229f1b3b28f578ba9d5d97fc30b98
Han ByoungJu <onemorebottlee@gmail.com>
2023-05-24 10:23:16 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,21 +1,3 @@
 function solution(my_string) {
-    let result = 0
-    let number = ""
-    
-    for(let i = 0; i < my_string.length; i++){
-      	const ele = my_string[i]
-        
-        if(!isNaN(Number(ele))){
-          number += ele
-          
-          if(i === my_string.length-1){
-            result += number*1
-          }
-        }else{
-          result += number*1
-          number = ""
-        }
-    }
-  
-    return result
+    return my_string.replace(/[A-z]/g, " ").split(" ").map(Number).reduce((acc,cur) => acc+cur,0)
 }
