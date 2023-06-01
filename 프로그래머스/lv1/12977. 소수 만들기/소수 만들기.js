e60004898811b5712fc57711334384423bc6ac71
Han ByoungJu <onemorebottlee@gmail.com>
2023-06-01 10:46:28 +0900

    [level 1] Title: 소수 만들기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
@@ -1,16 +1,4 @@
 function solution(nums) {
-  	// 서로 다른 3개의 수를 더해 나오는 수를 찾아 중복없이 모으기
-  	const sumNum = []
-  	for(let i = 0; i < nums.length-2; i++){
-        for(let j = 1; j < nums.length-1; j++){
-            for(let k = 2; k < nums.length; k++){
-                if(i < j && j < k && i < k){
-                    sumNum.push(nums[i] + nums[j] + nums[k])
-                }
-            }
-        }
-    }
-  
   	// 소수 체크
   	const isPrime = (num) => {
         if(num <= 1)return false
@@ -19,13 +7,20 @@ function solution(nums) {
             }
         return true    
   	}
-    
-  	// 3개를 더한 수가 소수인지 체크해 소수만 남기기
-  	const primeArr = []
-  	for(let num of sumNum){
-  			if(isPrime(num))primeArr.push(num)
-  	}
+  
+  	// 서로 다른 3개의 수를 더해 나오는 수를 찾아 중복없이 모으기
+  	const sumNum = []
+  	for(let i = 0; i < nums.length-2; i++){
+        for(let j = 1; j < nums.length-1; j++){
+            for(let k = 2; k < nums.length; k++){
+                if(i < j && j < k && i < k){
+                  	// 3개를 더한 수가 소수인지 체크해 소수만 남기기
+                  	if(isPrime(nums[i] + nums[j] + nums[k])) sumNum.push(isPrime(nums[i] + nums[j] + nums[k]))
+                }
+            }
+        }
+    }
     
   	// 소수의 개수 세기
-  	return primeArr.length
+  	return sumNum.length
 }
