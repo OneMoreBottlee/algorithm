03e353856ab40f32fea0380326b0093952fd9007
Han ByoungJu <onemorebottlee@gmail.com>
2023-09-15 13:53:00 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -0,0 +1,9 @@
+-- 코드를 입력하세요
+-- 보호 시작일 INS.DATETIME 보다 입양일 OUTS.DATETIME 이 더 빠른
+-- 동물의 아이디와 이름을 조회하는 SQL문을 작성해주세요.
+-- 이때 결과는 보호 시작일이 빠른 순으로 조회해야합니다.
+SELECT INS.ANIMAL_ID, INS.NAME
+FROM ANIMAL_INS INS INNER JOIN ANIMAL_OUTS OUTS
+ON INS.ANIMAL_ID = OUTS.ANIMAL_ID 
+WHERE INS.DATETIME > OUTS.DATETIME
+ORDER BY INS.DATETIME
